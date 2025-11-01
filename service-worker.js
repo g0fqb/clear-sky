bcca69171445028aeeea63b7a54f8e763d6f97b8
g0fqb <g0fqb1@gmail.com>
2025-11-01 11:06:47 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,15 +2,13 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('clear-sky-v1').then(cache => {
       return cache.addAll([
-        '/',                  // Root
         '/index.html',        // Main page
         '/app.js',            // Your script
         '/styles.css',        // Your stylesheet
         '/manifest.json',     // Web app manifest
         '/icon-192.png',      // Icon (must exist)
         '/icon-512.png',     // Icon (must exist)
-        '/favicon.ico',      // Icon (must exist)
-        '/server.js'       // Icon (must exist)
+        '/favicon.ico'      // Icon (must exist)
         
         // Do NOT include favicon.ico unless it's present
       ]);
