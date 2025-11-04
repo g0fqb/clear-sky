a12d3da332ea2be6a117961021d912cdef19ad36
g0fqb <g0fqb1@gmail.com>
2025-11-04 13:05:53 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -33,3 +33,17 @@ self.addEventListener('install', event => {
     })
   );
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
